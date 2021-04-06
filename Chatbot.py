a1d7b2b794649d0345ffec66fa944ce3b9fd9d2d
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-06 23:20:40 +0500

    Updated To Resolve An Error

--- Chatbot.py
+++ Chatbot.py
@@ -47,48 +47,47 @@ print(len(words), "unique lemmatized words", words)
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
+# # Building Deep Learning Model
+# training = []
+# output_empty = [0] * len(classes)
+# for doc in documents:
+#     # Intializing Bag Of Words
+#     bag = []
+#     # List Of Tokenized Words For Patterns
+#     pattern_words = doc[0]
+#     # Lemmatize Each Word - Create Base Word, In Attempt Related To Represent Related Words
+#     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+#     # Create Our Bag Of Words Array With 1, If Word Match Found In Current Position
+#     for w in words:
+#         bag.append(1) if w in pattern_words else bag.append(0)
 
-# Building Deep Learning Model
-training = []
-output_empty = [0] * len(classes)
-for doc in documents:
-    # Intializing Bag Of Words
-    bag = []
-    # List Of Tokenized Words For Patterns
-    pattern_words = doc[0]
-    # Lemmatize Each Word - Create Base Word, In Attempt Related To Represent Related Words
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # Create Our Bag Of Words Array With 1, If Word Match Found In Current Position
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
+#     # Output is '0' For Each Tag And '1' For Current Tag (For Each Pattern)
+#     output_row = list(output_empty)
+#     output_row[classes.index(doc[1])] = 1
 
-    # Output is '0' For Each Tag And '1' For Current Tag (For Each Pattern)
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+#     training.append(bag, output_row)
 
-    training.append(bag, output_row)
+# # Shuffle Our Features And Turn Into Numpy Array
+# random.shuffle(training)
+# training  = np.array(training)
+# # Create Train And Test Lists: X - Patterns, Y - Intents
+# train_x = list(training[:,0])
+# train_y = list(training[:,1])
+# print("Training Data Created")
 
-# Shuffle Our Features And Turn Into Numpy Array
-random.shuffle(training)
-training  = np.array(training)
-# Create Train And Test Lists: X - Patterns, Y - Intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-print("Training Data Created")
+# # Creating Model
+# model = Sequential()
+# model.add(Dense(128, activation = 'relu', input_shape = (len(train_x[0],))))
+# model.add(Dropout(0.5))
+# model.add(Dense(64, activation = 'relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(len(train_y[0], activation = 'softmax')))
 
-# Creating Model
-model = Sequential()
-model.add(Dense(128, activation = 'relu', input_shape = (len(train_x[0],))))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation = 'relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0], activation = 'softmax')))
+# # Compilation Step
+# sgd = SGD(lr = 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
+# model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+# model.summary()
 
-# Compilation Step
-sgd = SGD(lr = 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
-model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
-model.summary()
-
-# Fitting Model On 200 Epochs
-hist = model.fit(np.array(trian_x), np.array(train_y), epochs = 200, batch_size = 5, verbose = 1)
-model.save('Chatbot_Deep_Learning_Model.h5', hist)
+# # Fitting Model On 200 Epochs
+# hist = model.fit(np.array(trian_x), np.array(train_y), epochs = 200, batch_size = 5, verbose = 1)
+# model.save('Chatbot_Deep_Learning_Model.h5', hist)

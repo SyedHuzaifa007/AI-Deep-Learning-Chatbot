2b6bc6a48f754606c9491714c2a83589c41df67f
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-06 13:31:44 +0500

    Build Deep Learning Model

--- Chatbot.py
+++ Chatbot.py
@@ -46,3 +46,32 @@ print(len(classes), "classes", classes)
 print(len(words), "unique lemmatized words", words)
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
+
+
+# Building Deep Learning Model
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    # Intializing Bag Of Words
+    bag = []
+    # List Of Tokenized Words For Patterns
+    pattern_words = doc[0]
+    # Lemmatize Each Word - Create Base Word, In Attempt Related To Represent Related Words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # Create Our Bag Of Words Array With 1, If Word Match Found In Current Position
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # Output is '0' For Each Tag And '1' For Current Tag (For Each Pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append(bag, output_row)
+
+# Shuffle Our Features And Turn Into Numpy Array
+random.shuffle(training)
+training  = np.array(training)
+# Create Train And Test Lists: X - Patterns, Y - Intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training Data Created")

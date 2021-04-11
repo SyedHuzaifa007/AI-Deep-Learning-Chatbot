2490a7c30cfcd50b168225b43272e0c1a41a19a9
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-12 00:03:10 +0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -58,22 +58,22 @@ for doc in documents:
     # Lemmatized Each Word - Create Base Word, In Attempt To Represent Related Words
     patern_words = [lemmatizer.lemmatize(words.lower()) for word in pattern_words]
 # Create Our Bag Of Words Array With 1, If Word Match Found In Current Pattern
-        for w in words:
-            bag.append(1) if w in patern_words else bag.append(0)
+    for w in words:
+        bag.append(1) if w in patern_words else bag.append(0)
 
-            # Output Is A '0' For Each Tag and '1' for current tag (for each pattern)
-            output_row = list(output_empty)
-            output_row[classe.index(doc[1])] = 1
+        # Output Is A '0' For Each Tag and '1' for current tag (for each pattern)
+        output_row = list(output_empty)
+        output_row[classe.index(doc[1])] = 1
 
-            training.append([bag, output_row])
+        training.append([bag, output_row])
 
-            # Shuffle Our Features And Turn Into np.array
-            random.shuffle(taraning)
-            training = np.array(training)
+        # Shuffle Our Features And Turn Into np.array
+        random.shuffle(taraning)
+        training = np.array(training)
             
-            # Create Train And Tests Lists. X - patterns, Y - intents
-            train_x = list(training[:,0])
-            train_y = list(training[:,1])
-            print("Training Data Created")
+        # Create Train And Tests Lists. X - patterns, Y - intents
+        train_x = list(training[:,0])
+        train_y = list(training[:,1])
+        print("Training Data Created")
 
-            
+
